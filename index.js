80f38d7c7c6c26e795cc344eaa7a0df83e2404ff
Luuk-Meier-21 <luuk.meier@outlook.com>
2022-07-05 11:24:55 +0200

    nodemon added

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ arduino.onReady((sp, parser) => {
 
     // Connection to Arduino via serialport
     let parserCreated = false;
-    const interface = spawn("npm", ["run", "interface"]);
-
-    interface.on("spawn", () => {
-        open("http://localhost:8080/", {});
-    });
+    // const interface = spawn("npm", ["run", "interface"]);
+    // interface.on("spawn", (err) => {
+    //     if (err) console.log(err);
+    //     open("http://localhost:8080/", {});
+    // });
     // TODO: data leak from eventlisteners when refreshing to many times.
     // On every refresh and connection to the websocket:
     ws.on("connection", async (ws) => {
@@ -34,7 +34,7 @@ arduino.onReady((sp, parser) => {
 
         // Listen for messages from clientside
         ws.on("message", (message) => {
-            
+            console.log(`Client: ${message}`);
             switch (message.toString()) {
                 case "OPEN":
                     c.openSerial();
@@ -46,7 +46,9 @@ arduino.onReady((sp, parser) => {
                     sp.write(message);
             }
         });
+
         parser.on("data", (data) => {
+            console.log(`Serial: ${data}`)
             ws.send(data.toString());
         });
     });
